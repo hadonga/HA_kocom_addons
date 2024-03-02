6f69e375dd0c96d5a27aaf0299ab346a7fa44c98
hadonga <hadonga@gmail.com>
2024-03-02 23:08:24 +0900

    update

--- kocomRS485/run.sh
+++ kocomRS485/run.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-SHARE_DIR=/share/kocom
+# SHARE_DIR=/share/kocom
 
-if [ ! -f $SHARE_DIR/rs485.py ]; then
-	mkdir $SHARE_DIR
-	mv /rs485.py $SHARE_DIR
+# if [ ! -f $SHARE_DIR/rs485.py ]; then
+# 	mkdir $SHARE_DIR
+# 	mv /rs485.py $SHARE_DIR
+# fi
+# /makeconf.sh
+
+# echo "[Info] Run Kocom Wallpad Controller For RS485 Ver.2024"
+# cd $SHARE_DIR
+# python3 $SHARE_DIR/rs485.py
+
+# # for dev
+# while true; do echo "#### Wallpad Monitoring ... ####"; sleep 128; done
+
+SHARE_DIR=/share
+
+if [ ! -f $SHARE_DIR/kocom.cfg ]; then
+	mv /kocom.cfg $SHARE_DIR
 fi
-/makeconf.sh
 
-echo "[Info] Run Kocom Wallpad Controller For RS485 Ver.2024"
+echo "[Info] Run Kocom Wallpad with RS485..."
+
 cd $SHARE_DIR
-python3 $SHARE_DIR/rs485.py
-
-# for dev
-while true; do echo "#### Wallpad Monitoring ... ####"; sleep 128; done
+python3 /kocom.py
