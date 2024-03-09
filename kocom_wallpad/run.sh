b6aa8ea69decd6253b88c28e7ccfb78c0f930480
Dong HE <hadonga@gmail.com>
2024-03-09 11:03:26 +0900

    Update run.sh

--- kocom_wallpad/run.sh
+++ kocom_wallpad/run.sh
@@ -12,17 +12,15 @@
 # cd $SHARE_DIR
 # python3 $SHARE_DIR/rs485.py
 
-# # for dev
-# while true; do echo "#### Wallpad Monitoring ... ####"; sleep 128; done
 
 SHARE_DIR=/share
 
-if [ ! -f $SHARE_DIR/kocom.cfg ]; then
-echo "[Info] copy files to the share folder !"
+echo "[Info] copy files to the share folder!"
 	cp /kocom.cfg $SHARE_DIR
  	cp /kocom.py $SHARE_DIR
-fi
 
-echo "[Info] Run Kocom Wallpad with RS485..."
-cd $SHARE_DIR
+echo "[Info] Run Kocom Wallpad controller with RS485 Ver.2024 ..."
 python3 $SHARE_DIR/kocom.py
+
+# # for dev
+# while true; do echo "#### Keep Container Alive ####"; sleep 128; done
